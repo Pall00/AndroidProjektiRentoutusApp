0e17461572534a0c82053ca7a490b9d582d9cfc7
Pall00 <juhoahol@gmail.com>
2021-11-26 17:25:55 +0200

    Added relaxin activity.

---
+++ app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
@@ -0,0 +1,14 @@
+package com.example.androidprojectirentoutusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Relaxing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relaxing);
+    }
+}
