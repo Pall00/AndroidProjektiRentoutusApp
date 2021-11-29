aed992b1b02f39ffd88d939f4722b388eca5aee0
Pall00 <juhoahol@gmail.com>
2021-11-29 11:55:01 +0200

    Jotain uutta!

---
+++ app/src/main/java/com/example/androidprojectirentoutusapp/User.java
@@ -0,0 +1,17 @@
+package com.example.androidprojectirentoutusapp;
+
+public class User {
+    private int age;
+    private int weight;
+    private int height;
+    private String name;
+    private int level;
+
+    public User(int age, int weight, int height, String name, int level) {
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.name = name;
+        this.level = level;
+    }
+}
