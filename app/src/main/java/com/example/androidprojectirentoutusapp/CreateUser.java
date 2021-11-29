258663ab98f0f6ed4fd40e3ae30ef4ad1b3bb249
Pall00 <juhoahol@gmail.com>
2021-11-29 12:56:50 +0200

    Lisäsin toiminnallisuuden rekisteröi nappulaan!

--- app/src/main/java/com/example/androidprojectirentoutusapp/CreateUser.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/CreateUser.java
@@ -3,12 +3,29 @@ package com.example.androidprojectirentoutusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CreateUser extends AppCompatActivity {
+    private EditText eName;
+    private EditText eAge;
+    private EditText eWeight;
+    private EditText eHeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
+        eName = findViewById(R.id.etName);
+        eAge = findViewById(R.id.etAge);
+        eHeight = findViewById(R.id.etHeight);
+        eWeight = findViewById(R.id.etWeight);
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/androidprojectirentoutusapp/MainActivity.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.androidprojectirentoutusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button eNewUser;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eNewUser = findViewById(R.id.btnCreateNewUser);
+
+        if(sharedPreferences != null){
+
+        }
+
     }
+    public void createUser(View v){
+        Log.i("MY_APP", "createUser()");
+        Intent intent = new Intent(this, CreateUser.class);
+
+        startActivity(intent);
+    }
+
 }
