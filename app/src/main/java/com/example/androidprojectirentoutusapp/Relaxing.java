6abb863cdfb4fe2386c6ae130a80134eaf05f77f
Pall00 <juhoahol@gmail.com>
2021-11-30 12:15:41 +0200

    Added timer in relaxin. Commit

--- app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
@@ -3,13 +3,17 @@ package com.example.androidprojectirentoutusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Relaxing extends AppCompatActivity {
 
     private ImageButton imageButton;
+    private CountDownTimer countDownTimer;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,21 @@ public class Relaxing extends AppCompatActivity {
 
     public void imageButton(View v){
         View view = findViewById(R.id.imageButton);
-        Toast.makeText(getApplicationContext(), "Hienosti meditoitu", Toast.LENGTH_SHORT).show();
+        textView = findViewById(R.id.tvTimer);
 
+        Toast.makeText(getApplicationContext(), "Minuutti", Toast.LENGTH_SHORT).show();
+        CountDownTimer timer = new CountDownTimer(600000,1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText("seconds remaining: " + millisUntilFinished / 1000);
+
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setText("Valmis");
+            }
+        }.start();
     }
 }
