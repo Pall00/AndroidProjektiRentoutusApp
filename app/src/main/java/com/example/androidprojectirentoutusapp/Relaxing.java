ccf93066af20ce0faeb2e9740a44d464670ca6b8
Pall00 <juhoahol@gmail.com>
2021-11-30 12:00:16 +0200

    Commit

--- app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
@@ -3,12 +3,23 @@ package com.example.androidprojectirentoutusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Relaxing extends AppCompatActivity {
+
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relaxing);
+    }
+
+    public void imageButton(View v){
+        View view = findViewById(R.id.imageButton);
+        Toast.makeText(getApplicationContext(), "Hienosti meditoitu", Toast.LENGTH_SHORT).show();
+
     }
 }
