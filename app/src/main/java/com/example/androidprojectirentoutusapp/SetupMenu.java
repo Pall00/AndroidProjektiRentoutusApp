3688bb33458bfb8e46f94ba12818734cddd523d8
Santeri Rytkönen <[rytkonen.santeri123@gmail.com]>
2021-12-01 03:32:23 +0200

    Fixed exit method

--- app/src/main/java/com/example/androidprojectirentoutusapp/SetupMenu.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/SetupMenu.java
@@ -67,10 +67,9 @@ public class SetupMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 myexitDialog.dismiss();
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                finishAffinity();
+                System.exit(0);
+
             }
         });
         exittoLogin.setOnClickListener(new View.OnClickListener() {
