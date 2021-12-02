d235a31d38231a6f86d9ecb0d44edf823d8332d4
Pall00 <juhoahol@gmail.com>
2021-12-02 14:46:34 +0200

    Trivia activity working now.

--- app/src/main/java/com/example/androidprojectirentoutusapp/activity_trivia.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/activity_trivia.java
@@ -17,56 +17,52 @@ import java.util.List;
 public class activity_trivia extends AppCompatActivity {
 
     private ListView listView;
+    private ArrayAdapter<Trivia> listAdapter;
 
-    private ArrayList<String> lista;
-
+    public static  final String EXTRA = "com.example.androidprojectirentoutusapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
-
-        lista = new ArrayList<>();
 
         listView = findViewById(R.id.triviaListView);
 
         Log.i("MY_APP", User.getInstance().getName());
-
-
         Log.i("MY_APP", "käyttäjä nyt : " + User.getInstance().getName() + " ikä: " + User.getInstance().getAge());
+
 
         for(int i = 0; i<=User.getInstance().getLevel();i++){
             if(i==1){
-                lista.add("Moi1");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(1));
             }
             if(i==2){
-                lista.add("Moi2");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(2));
             }
             if(i==3){
-                lista.add("Moi3");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(3));
             }
             if(i==4){
-                lista.add("Moi4");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(4));
             }
             if(i==5){
-                lista.add("Moi5");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(5));
             }
             if(i==6){
-                lista.add("Moi6");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(6));
             }
             if(i==7){
-                lista.add("Moi7");
+                GlobalTrivias.getInstance().getTriviaList().add(new Trivia(7));
             }
         }
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista));
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, GlobalTrivias.getInstance().getTriviaList()));
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-                //Intent nextActivity = new Intent(MainActivity.this, PresidentDetailsActivity.class);
-                //nextActivity.putExtra(EXTRA, i);
-                //startActivity(nextActivity);
+                Intent nextActivity = new Intent(activity_trivia.this, TriviaDetailsActivity.class);
+                nextActivity.putExtra(EXTRA, i);
+                startActivity(nextActivity);
             }
         });
     }
