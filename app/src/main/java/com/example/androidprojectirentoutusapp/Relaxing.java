048a71659979d3c97b850e03841c44558f658f24
Pall00 <juhoahol@gmail.com>
2021-12-08 20:55:18 +0200

    Added pictures for relaxmode. Commit!

--- app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/Relaxing.java
@@ -88,28 +88,28 @@ public class Relaxing extends AppCompatActivity {
         ImageView  img = (ImageView) findViewById(R.id.imageView);
         switch(level){
             case 0:
-                img.setImageResource(R.drawable.aavikkotausta);
+                img.setImageResource(R.drawable.level1);
                 break;
             case 1:
-                img.setImageResource(R.drawable.level1test);
+                img.setImageResource(R.drawable.level2);
                 break;
             case 2:
-                img.setImageResource(R.drawable.level2test);
+                img.setImageResource(R.drawable.level3);
                 break;
             case 3:
-                img.setImageResource(R.drawable.level3test);
+                img.setImageResource(R.drawable.level4);
                 break;
             case 4:
-                img.setImageResource(R.drawable.level4test);
+                img.setImageResource(R.drawable.level5);
                 break;
             case 5:
-                img.setImageResource(R.drawable.level5test);
+                img.setImageResource(R.drawable.level6);
                 break;
             case 6:
-                img.setImageResource(R.drawable.level6test);
+                img.setImageResource(R.drawable.level7);
                 break;
             case 7:
-                img.setImageResource(R.drawable.level7test);
+                img.setImageResource(R.drawable.level7);
                 break;
         }
     }
