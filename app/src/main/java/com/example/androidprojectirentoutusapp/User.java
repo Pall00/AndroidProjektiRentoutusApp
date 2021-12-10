b5896118ac2ec6b26f641602f9054d41a1cbc77c
Pall00 <juhoahol@gmail.com>
2021-12-10 13:23:16 +0200

    Added alljavadoc lines in User class. Commit!

--- app/src/main/java/com/example/androidprojectirentoutusapp/User.java
+++ app/src/main/java/com/example/androidprojectirentoutusapp/User.java
@@ -1,11 +1,13 @@
 package com.example.androidprojectirentoutusapp;
+/**
+ *
+ * @author Juho
+ */
 
 public class User {
     /**
-     * Have variables age,weight height, name, level, relaxingTime,id and timer.
-     *
-     *
-     *
+     * This is a class for user.
+     * It have variables age,weight height, name, level, relaxingTime,id and timer.
      */
     private int age;
     private int weight;
@@ -17,6 +19,9 @@ public class User {
     private int id;
     private long timer;
 
+    /**
+     * This is constructor for creating new user.
+     */
     private User(){
     }
 
@@ -24,6 +29,10 @@ public class User {
         return ourInstance;
     }
 
+    /**
+     * This is a method for setting values to user.
+     * It have variables age,weight height, name, level, relaxingTime,id and timer.
+     */
     public void setValues(String name, int age, int weight, int height, int level, int relaxingTime, int id, long timer) {
         this.age = age;
         this.weight = weight;
@@ -35,6 +44,11 @@ public class User {
         this.timer = timer;
     }
 
+    /**
+     * This is a method for user to count and return bmi.
+     * It uses weight and height to calculate bmi.
+     *After bmi is calculated it will return value bmi.
+     */
     public double getBmi(){
 
         double weight = this.weight;
@@ -46,71 +60,105 @@ public class User {
         return bmi;
 
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getId(){
-        return this.id;
-    }
-
+    /**
+     * This is a method for user to levelUp.
+     * It checks what level user is and if user is under 6
+     * it will level user one level higher.
+     */
     public void levelUp(){
         if(this.getLevel()<6) {
             this.level += 1;
         }
     }
-
+    /**
+     * This is a method for resetting users level and relaxingTime back to zero.
+     */
     public void resetData(){
         this.level = 0;
         this.relaxingTime = 0;
         //this.timer = 60000;
     }
-
+    /**
+     * This is a method for returning value of users id variable.
+     */
+    public int getId(){
+        return this.id;
+    }
+    /**
+     * This is a method for returning value of users timer variable.
+     */
     public long getTimer(){
         return timer;
     }
-
+    /**
+     * This is a method for setting value of users timer variable.
+     */
     public void setTimer(long timer){
         this.timer = timer;
     }
-
+    /**
+     * This is a method for returning value of users age variable.
+     */
+    public int getAge() {
+        return age;
+    }
+    /**
+     * This is a method for setting value of users age variable.
+     */
     public void setAge(int age) {
         this.age = age;
     }
-
+    /**
+     * This is a method for returning value of users weight variable.
+     */
     public int getWeight() {
         return weight;
     }
-
+    /**
+     * This is a method for setting value of users weight variable.
+     */
     public void setWeight(int weight) {
         this.weight = weight;
     }
-
+    /**
+     * This is a method for returning value of users height variable.
+     */
     public int getHeight() {
         return height;
     }
-
+    /**
+     * This is a method for setting value of users height variable.
+     */
     public void setHeight(int height) {
         this.height = height;
     }
-
+    /**
+     * This is a method for returning value of users name variable.
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * This is a method for setting value of users name variable.
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * This is a method for returning value of users level variable.
+     */
     public int getLevel() {
         return level;
     }
-
+    /**
+     * This is a method for returning value of users relaxingTime variable.
+     */
     public int getRelaxingTime() {
         return relaxingTime;
     }
-
+    /**
+     * This is a method for setting value of users relaxingTime variable.
+     */
     public void addRelaxingTime(int time) {
         this.relaxingTime += time;
     }
